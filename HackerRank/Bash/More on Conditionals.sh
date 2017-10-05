c0e0f480884210847220aa1ff10d66a2714ff5a9
Varun Upadhyay <varun.u28@gmail.com>
2017-10-05 09:57:59 -0700

    Solved a problem

---
+++ HackerRank/Bash/More on Conditionals.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read a
+read b
+read c
+
+if [ $a == $b ] && [ $b == $c ]; then
+	echo "EQUILATERAL"
+elif [ $a != $b ] && [ $b != $c ] && [ $a != $c ]; then
+	echo "SCALENE"
+else 
+	echo "ISOSCELES"
+fi
