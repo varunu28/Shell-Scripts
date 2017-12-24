ecda27c33ac689855daf54defa56b42410eff000
Varun Upadhyay <varun.u28@gmail.com>
2017-12-23 21:57:47 -0800

    Update sqoop_import.sh

--- SQOOP_Import_Script/sqoop_import.sh
+++ SQOOP_Import_Script/sqoop_import.sh
@@ -8,14 +8,35 @@ while read -r line
 do
     name="$line"
 
-    sqoop import \
-    --connect jdbc:mysql://ms.itversity.com:3306/retail_db \
+    query="DESCRIBE $name"
+
+    echo "Importing $name"
+
+    sqoop eval --connect jdbc:mysql://ms.itversity.com:3306/retail_db \
     --username retail_user \
     --password itversity \
-    --table $name \
-    --warehouse-dir /user/varunu28/workshop \
-    1>/home/varunu28/sqoopLog/$name.log \
-    2>/home/varunu28/sqoopLog/$name.err
+    --query "${query}" 1>eval.out 2>eval.err
 
-    echo "$name"
+    if grep -q 'PRI' eval.out ; then
+        sqoop import \
+        --connect jdbc:mysql://ms.itversity.com:3306/retail_db \
+        --username retail_user \
+        --password itversity \
+        --table $name \
+        --warehouse-dir /user/varunu28/workshop \
+        1>/home/varunu28/sqoopLog/$name.log \
+        2>/home/varunu28/sqoopLog/$name.err
+    else    
+        sqoop import \
+        --connect jdbc:mysql://ms.itversity.com:3306/retail_db \
+        --username retail_user \
+        --password itversity \
+        --table $name \
+        --num-mappers 1 \
+        --warehouse-dir /user/varunu28/workshop \
+        1>/home/varunu28/sqoopLog/$name.log \
+        2>/home/varunu28/sqoopLog/$name.err
+    fi
+
+    rm eval.out eval.err 
 done < "$filename"
