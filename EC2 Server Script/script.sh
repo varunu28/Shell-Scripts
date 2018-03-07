997f7d90dc3ed4ff4b025ca6b4f6917cd4395cca
Varun Upadhyay <varun.u28@gmail.com>
2018-03-07 08:37:44 -0800

    Update script.sh

--- EC2 Server Script/script.sh
+++ EC2 Server Script/script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+sudo su
 yum update -y
 yum install httpd -y
+# Change the name of s3 bucket to your bucket name
+aws s3 cp s3://mywebsitebucket-varun-here /var/www/html/ --recursive
 service httpd start
 chkconfig httpd on
-# Change the name of s3 bucket to your bucket name
-aws s3 cp s3://mywebsitebucket-varun-here/index.html /var/www/html
